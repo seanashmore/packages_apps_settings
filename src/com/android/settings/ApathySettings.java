a2243826a15c0ed1ecb9baa4cf1fbe0168266660
Sean Ashmore <seanashmore@outlook.com>
2015-07-16 21:09:47 +0100

    Enable notification count option

--- src/com/android/settings/ApathySettings.java
+++ src/com/android/settings/ApathySettings.java
@@ -15,8 +15,10 @@ public class ApathySettings extends SettingsPreferenceFragment implements Prefer
     private static final String TAG = "ApathySettings";
 
     private final String KEY_BATTERY_PERCENTAGE = "show_battery_percentage";
+    private final String KEY_NOTIFICATION_COUNT = "show_notification_count";
 
     private SwitchPreference mBatteryPercentagePreference;
+    private SwitchPreference mShowNotificationCountPreference;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -24,12 +26,19 @@ public class ApathySettings extends SettingsPreferenceFragment implements Prefer
         addPreferencesFromResource(R.xml.apathy_settings);
 
         mBatteryPercentagePreference = findAndInitSwitchPref(KEY_BATTERY_PERCENTAGE);
-
         if(mBatteryPercentagePreference != null){
             Log.i(TAG,"Found battery percentage switch preference OK");
             mBatteryPercentagePreference.setOnPreferenceChangeListener(this);
         }else{
             Log.i(TAG,"Something went wrong. Couldn't find battery percentage switch preference");
+        }
+
+        mShowNotificationCountPreference = findAndInitSwitchPref(KEY_NOTIFICATION_COUNT);
+        if(mShowNotificationCountPreference != null){
+            Log.i(TAG,"Found notification count switch preference OK");
+            mShowNotificationCountPreference.setOnPreferenceChangeListener(this);
+        }else{
+            Log.i(TAG,"Something went wrong. Couldn't find notification count switch preference");
         }
 
         updateAllOptions();
@@ -59,6 +68,14 @@ public class ApathySettings extends SettingsPreferenceFragment implements Prefer
                         mBatteryPercentagePreference.isChecked() ? 1 : 0);
                 Log.i(TAG, "Set batteryPercentagePreference to : " + mBatteryPercentagePreference.isChecked());
             }
+        }else if(preference == mShowNotificationCountPreference){
+            if(mShowNotificationCountPreference != null) {
+                Log.i(TAG, "notificationCount clicked");
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.STATUS_BAR_SHOW_NOTIFICATION_COUNT,
+                        mShowNotificationCountPreference.isChecked() ? 1 : 0);
+                Log.i(TAG, "Set notificationCount to : " + mShowNotificationCountPreference.isChecked());
+            }
         }else{
             return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
@@ -77,11 +94,19 @@ public class ApathySettings extends SettingsPreferenceFragment implements Prefer
 
     private void updateAllOptions(){
         Log.i(TAG, "updateAllOptions()");
+
         if(mBatteryPercentagePreference != null) {
             Log.i(TAG,"batteryPercentagePreference is not null. Updating switch preference");
             updateSwitchPreference(mBatteryPercentagePreference,
                     Settings.System.getInt(getContentResolver(),
                             Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENTAGE , 0) != 0);
+        }
+
+        if(mShowNotificationCountPreference != null) {
+            Log.i(TAG,"showNotificationCount is not null. Updating switch preference");
+            updateSwitchPreference(mShowNotificationCountPreference,
+                    Settings.System.getInt(getContentResolver(),
+                            Settings.System.STATUS_BAR_SHOW_NOTIFICATION_COUNT , 0) != 0);
         }
     }
 
@@ -101,6 +126,15 @@ public class ApathySettings extends SettingsPreferenceFragment implements Prefer
                         Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENTAGE, value);
             }catch(NumberFormatException e){
                 Log.e(TAG,"Could not persist battery percentage display setting");
+            }
+            return true;
+        }else if(key == KEY_NOTIFICATION_COUNT){
+            try{
+                int value = Integer.parseInt((String) objValue);
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.STATUS_BAR_SHOW_NOTIFICATION_COUNT, value);
+            }catch(NumberFormatException e){
+                Log.e(TAG,"Could not persist notification count setting");
             }
             return true;
         }
